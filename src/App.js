603e6d4abbb581977fec4c9f1b2e70d59c0a0605
DesyRahmadani <desyrahmadani268@gmail.com>
2022-03-23 23:23:00 +0700

    Tugas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './Music';
 
 function App() {
+  const handClick= () => {
+    console.log("Selected")
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Play Music">
+          <h2>Music</h2>
+          <img src={data.album.images[0].url}/>
+          <h2>{data.album.name}</h2>
+          <h3>{data.album.artists[0].name}</h3>
+          <button onClick={handClick}>Yess</button>
+        </div>
       </header>
     </div>
   );
